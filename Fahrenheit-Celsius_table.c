8b7b6cd796412aea537535c8bc607d303262962e
Raghav Sharma <raghav_tinker@yahoo.com>
2020-09-16 12:27:49 +0530

    Making the table neater.
    Removed a debugging statement.

--- Fahrenheit-Celsius_table.c
+++ Fahrenheit-Celsius_table.c
@@ -15,11 +15,13 @@ main(){
     step = 20; /*Step size*/
 
     fahr = lower;
-
+    
+    printf("Fahrenheit-Celsius Conversion Table\n");
+    printf("===================================\n");
     while (fahr <= upper){
         celsius = (5.0/9.0) * (fahr-32.0);
+        printf("-------------------------------\n");
         printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + 20;
     }
-    printf("%%9\n");
 }
