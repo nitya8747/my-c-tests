89ec15f5564aa309a08970c7a0f135c16d0a18ba
Raghav Sharma <raghav_tinker@yahoo.com>
2020-09-17 00:19:59 +0530

    Added capability of finding number of characters put in input
    Learnt the short form of incrementation : ++<variableName>

--- CopyInputOutput.c
+++ CopyInputOutput.c
@@ -2,13 +2,13 @@
 main(){
     int c;
     c = getchar();
-    
+    long number_of_characters;
     while (c != EOF){
         putchar(c);
         printf("%d\n",c);
         c = getchar();
-        
+        ++number_of_characters; //Incrementing number_if_characters
     }
     //Prints the number of characters put in input
-    printf("HI");  //To invoke press CTRL + D or in windows CTRL +Z
+    printf("Number of characters: %ld\n", number_of_characters);  //To invoke press CTRL + D or in windows CTRL +Z
 }
