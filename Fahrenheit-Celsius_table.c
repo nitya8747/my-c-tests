515eddf9175dfc8f1b4de830f5da07cdec81c120
Raghav Sharma <raghav_tinker@yahoo.com>
2020-09-16 12:18:51 +0530

    Increasing accuracy of the application
    We changed the data type of the fahr and celsius variable from integer to floating point.
    
    This provides an accurate celsius values converted from fahrenheit.

--- Fahrenheit-Celsius_table.c
+++ Fahrenheit-Celsius_table.c
@@ -5,20 +5,21 @@ main(){
     /*
         Initiating variables required
     */
-    int fahr;
-    int celsius;
+    float fahr;
+    float celsius;
     /*OR for multiple variables*/
     int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
+    lower = 0;  /*Lower limit of temp table*/
+    upper = 300;/*Upper limit of temp table*/
+    step = 20; /*Step size*/
 
     fahr = lower;
 
     while (fahr <= upper){
-        celsius = 5*(fahr-32)/9;
-        printf("%3d\t%6d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + 20;
     }
+    printf("%%9\n");
 }
