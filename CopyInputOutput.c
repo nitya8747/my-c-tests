da6d5edf7283ae11226e1bce6f6e198f6a67d0f5
Raghav Sharma <raghav_tinker@yahoo.com>
2020-09-20 23:24:39 +0530

    Changed the long data type to float in the while loop
    Also added a for loop to do what while loop does.
    
    Learnt about null statement

--- CopyInputOutput.c
+++ CopyInputOutput.c
@@ -2,7 +2,7 @@
 main(){
     int c;
     c = getchar();
-    long number_of_characters;
+    float number_of_characters;
     while (c != EOF){
         putchar(c);
         printf("%d\n",c);
@@ -10,5 +10,16 @@ main(){
         ++number_of_characters; //Incrementing number_if_characters
     }
     //Prints the number of characters put in input
-    printf("Number of characters: %ld\n", number_of_characters);  //To invoke press CTRL + D or in windows CTRL +Z
+    printf("Number of characters: %0f\n", number_of_characters);  //To invoke press CTRL + D or in windows CTRL +Z
+
+    //Learning to implement above functionality in for loop
+
+    double nc; //This variable will give the number of characters in the input
+
+    printf("==========================\n");
+    printf("Implementing whatever while loop does in for loop\n");
+    for (nc = 0; getchar() != EOF; ++nc){
+        ; //Null Statement
+    }
+    printf("Number of characters: %0f\n", nc);
 }
