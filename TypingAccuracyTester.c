b367bc0f0d275a9428e799ecd7d8be29df190171
Raghav Sharma <raghav_tinker@yahoo.com>
2020-09-22 23:40:47 +0530

    Added % function as well.
    Please note: Not running on my terminal. Please fix it later on.
    
    PROBLEM: Backspace is not being dedected

--- TypingAccuracyTester.c
+++ TypingAccuracyTester.c
@@ -8,10 +8,18 @@ void main() {
     int keyboard;
     int backspace_number;
     int number_of_characters;
-    for (number_of_characters = 0; (keyboard = getchar()) != EOF; ++number_of_characters ){
+    number_of_characters = 0;
+    backspace_number = 0;
+
+    for (number_of_characters = 0; ((keyboard = getchar()) != EOF); ++number_of_characters ){
+        printf("%c %d", keyboard, keyboard);
         if (keyboard == '\b'){  //If the backspace key is pressed
-            backspace_number++;
+            backspace_number = backspace_number + 1;
+            printf("Backspace %d\n",backspace_number);
         }
+        
     }
-    printf("The probability of you hitting backspace is: %d\n",(backspace_number/number_of_characters));
-}
+    printf("The probability of you hitting backspace is: %d" , (backspace_number/number_of_characters));
+    printf("The percentage of you hitting backspace is: %d" , (backspace_number/number_of_characters)*100);
+
+}   
