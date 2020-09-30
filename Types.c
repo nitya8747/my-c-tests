83d06e5ca9aff655ca9d377848683e5fcf6743e9
Raghav Sharma <raghav_tinker@yahoo.com>
2020-10-01 00:57:18 +0530

    Learnt how to use enumerations and their power.

--- Types.c
+++ Types.c
@@ -5,8 +5,13 @@
 void main(){
 
     //String declaration
-    char string[MAXLINE] = "Raghav Sharma\0";  //String is basically a collection of characters hence declared as a character
+    char string[MAXLINE] = "Raghav Sharma\0";  //String is basically a collection of characters hence declared as a character. \0 is a null character
+
     printf("%s\n", string); //%s must be used to print the string
 
+    //Enumeration constant
+
+    enum months {JAN=1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC}; //Enumeration is just like define which creates a symbolic links to certain constants
+    printf("%d\n", JAN); //Calling the symbolic link
 }
 
