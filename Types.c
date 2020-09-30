66d770dad80b1c4dd38ebbd1846a7229b1167a47
Raghav Sharma <raghav_tinker@yahoo.com>
2020-10-01 00:48:56 +0530

    Learning Types in C
    a)Learnt how to declare a string
    v)Realised that string is basically an array of characters

---
+++ Types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+void main(){
+
+    //String declaration
+    char string[MAXLINE] = "Raghav Sharma\0";  //String is basically a collection of characters hence declared as a character
+    printf("%s\n", string); //%s must be used to print the string
+
+}
+
