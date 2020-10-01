03fc4365cdd20b7a844582d0588e70c3771bab54
Raghav Sharma <raghav_tinker@yahoo.com>
2020-10-01 09:14:07 +0530

    Took a string as an inpput and printed it

--- Types.c
+++ Types.c
@@ -9,8 +9,14 @@ void main(){
 
     printf("%s\n", string); //%s must be used to print the string
 
-    //Enumeration constant
+    //Using scanf to get an input
+    char line[MAXLINE];
+    printf("Please enter a line: ");
+    
+    scanf("%s", line);
+    printf("%s\n", line);
 
+    //Enumeration constant
     enum months {JAN=1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC}; //Enumeration is just like define which creates a symbolic links to certain constants
     printf("%d\n", JAN); //Calling the symbolic link
 }
