466933345f3898e8331a1de9ac477d76a0ba27ae
Raghav Sharma <raghav_tinker@yahoo.com>
2020-10-02 11:59:04 +0530

    Learnt how to convert types
    a)String of digits to integer
    
    b)Built a function to:
    	i)convert character from lowercase to upper case
    	ii)convert a character from upper case to lowercase
    	iii)Learnt about inbuilt functions that do that for you too
    	iv)Learnt about the 32 difference between lowercase and uppercase in ASCII chart

--- Types.c
+++ Types.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 #define MAXLINE 1000
 
 void main(){
@@ -19,5 +22,51 @@ void main(){
     //Enumeration constant
     enum months {JAN=1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC}; //Enumeration is just like define which creates a symbolic links to certain constants
     printf("%d\n", JAN); //Calling the symbolic link
+
+    //Conversion of types
+
+    //String of digits to integer
+    char str[] = "1234444";
+    int number =  atoi(str);
+    printf("%d\n", number);
+
+    //Lower case converter
+    printf("Converting 'R' to lowercase: %c\n", lower('R'));
+
+    //Upper case converter
+    printf("Converting 'r' to uppercase: %c\n", upper('r'));
+
+    //Upper case built in 
+    printf("Built in: %c\n", toupper('c'));
+
+    //Lower case built in 
+    printf("Built in: %c\n", tolower('c'));
+    int c;
+    c = 'A';
+    c= 65 + 32;
+    printf("%c\n", c);
 }
 
+
+//Converting a uppercase character to an lowercase character
+int lower (int c){
+    if (c >= 'A' && c <= 'Z'){
+        c = c + 32;  //There is a change of 32 to become a uppercase character
+        return c;
+    }
+    else{
+        return c;
+    }
+}
+
+
+//Convertin a lowercase character to uppercase character
+int upper(int c){
+    if (c >= 'a'  && c <='z'){
+        c = c - 32; //There is a change of 32 to become a lower case character
+        return c;
+    }
+    else{
+        return c;
+    }
+}
