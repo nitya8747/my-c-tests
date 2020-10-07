93007f3c17a5ce47f9e6c0372634ab85e3ba4dd1
Raghav Sharma <raghav_tinker@yahoo.com>
2020-10-07 17:43:39 +0530

    Created function countInputs()
    a)Counts number of each digit inputted
    
    b)Counts the number of whitespaces in input
    
    c)Used switch case statements to solve this problem

--- ControlFlow.c
+++ ControlFlow.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #define MAXLINE 1000
 
+//Function Declarations
+void countingInputs();  //Counts the number of digits, whitespaces, others
 void main(){
     int number1;
     int number2;
@@ -26,5 +28,59 @@ void main(){
         printf("ERROR!!!");
     }
     printf("\n");
+
+
+    //Switch case system
+    int number;
+    switch (number){
+        case 5:  printf("Its is 5\n");
+        default: printf("Error");
+    }
+
+    //Example 2
+    countingInputs();
+
+}
+
+void countingInputs(){
+    int c, i, nwhite, nother, ndigit[10];   //Input, increment , number of whitespaces, other character, digit array
+
+    nwhite = nother = 0;
+
+    //Setting all elements in array to 0
+    for (i = 0; i < 10; i++){
+        ndigit[i] = 0;
+    }
+    printf("\nEnter whatever you want to enter => ");
+    while ((c = getchar()) != EOF){
+        switch (c){
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                    ndigit[c - '0']++;
+
+            case ' ':
+            case '\n':
+            case '\t':
+                    nwhite++;
+            default:
+                nother++;
+                break;
+        }
+    }
+
+    printf("digits = ");
+    for (i = 0; i< 10; i++){
+        printf(" %d", ndigit[i]);
+    }
+
+    printf(", white spaces = %d, other = %d\n", nwhite, nother);
 
 }
