9c4484bdf5fbd4d713d848e58f21317e8bab39d8
Raghav Sharma <raghav_tinker@yahoo.com>
2020-11-07 01:14:00 +0530

    Push Pop file
    Its is for editingt he stack in the way the polish calculator works

---
+++ PolishCalculator/PushPop.c
@@ -0,0 +1,28 @@
+#define MAXVAL 100  /*maximum depth of val stack*/
+
+int stack_position = 0;  //NExt free stack position
+double value[MAXVAL];
+
+/*Push: push f onto value stack*/
+
+void push(double f){
+    if (stack_position < MAXVAL){
+        value[stack_position++] = f;
+    }
+    else{
+        printf("error: stack full, can't push %g\n", f);
+    }
+}
+
+
+//pop: pop and return top value from stack
+
+double pop(){
+    if(stack_position > 0){
+        return value[--stack_position];
+    }
+    else{
+        printf("error: stack empty \n");
+        return 0.0;
+    }
+}
