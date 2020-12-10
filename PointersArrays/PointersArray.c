4ba4f449e0683f58528d366ed7ab51f2a0cecec4
Raghav Sharma <raghav_tinker@yahoo.com>
2020-12-11 00:04:05 +0530

    Initialization of Pointer Arrays

--- PointersArrays/PointersArray.c
+++ PointersArrays/PointersArray.c
@@ -16,3 +16,16 @@ void main(){
         printf("%c    %d\n", *pointerArray[i], *pointerArray[i]);
     }
 }
+
+//Initialization of Pointer Arrays
+char *month_name(int n){
+    static char *name[] = {
+        "Illegal month",
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    };
+
+    return ( n < 1 || n > 12) ? name[0] : name[n];
+}
