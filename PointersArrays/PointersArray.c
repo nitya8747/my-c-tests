e4bf953e02fb77cf97fc764b04f9c5eebee252bc
Raghav Sharma <raghav_tinker@yahoo.com>
2020-12-10 08:15:42 +0530

    Manipulating a pointer Array

--- PointersArrays/PointersArray.c
+++ PointersArrays/PointersArray.c
@@ -1,1 +1,18 @@
 //arrays of pointers
+#include <stdio.h>
+
+void main(){
+
+    char *pointerArray[14];
+    char character = 'r';
+    char charArray[] = "Raghav Sharma";
+
+    int j = 0;
+    for (int i = 14; i >= 0; i--){
+        pointerArray[j++] = &charArray[i];
+    }
+
+    for (int i = 0; i < 15; i++){
+        printf("%c    %d\n", *pointerArray[i], *pointerArray[i]);
+    }
+}
